1074b1556aaedebf3077e86cd5b9cfafd48bfde0
anthowen <anthowen@users.noreply.github.com>
2023-01-03 20:18:10 -0500

    chore(fe): Change to mumbai chain

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,10 +1,10 @@
 import { getDefaultWallets } from '@rainbow-me/rainbowkit'
 import { configureChains, createClient } from 'wagmi'
-import { goerli, mainnet } from 'wagmi/chains'
+import { polygonMumbai } from 'wagmi/chains'
 import { publicProvider } from 'wagmi/providers/public'
 
 const { chains, provider, webSocketProvider } = configureChains(
-  [mainnet, ...(process.env.NODE_ENV === 'development' ? [goerli] : [])],
+  [polygonMumbai],
   [
     publicProvider(),
   ],
