8ff07550c177669a17d86bae41ae08200ded2cbe
anthowen <anthowen@users.noreply.github.com>
2023-01-18 07:07:41 -0500

    chore(app): Log RPC API call count

--- packages/app/src/pages/api/[events].ts
+++ packages/app/src/pages/api/[events].ts
@@ -3,7 +3,7 @@ import { BigNumber, ethers } from "ethers";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { eventContract } from "../../utils/contracts";
 import { convertFromHexString } from "../../utils/string";
-import { getRpcProvider } from '../../utils/rpc';
+import { getRpcProvider } from "../../utils/rpc";
 
 const FILTER_BLOCK_STEP = 3000;
 const topicIds: Record<string, string> = {
@@ -15,9 +15,10 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { events } = req.query;
   const eventType = events as string;
   if (!eventType || !topicIds[eventType]) {
-    return res.status(403).json({ message: 'Unidentified event type'});
+    return res.status(403).json({ message: "Unidentified event type" });
   }
 
+  let i = 0;
   try {
     const rpcProvider = getRpcProvider(process.env.RPC_URL);
     const endBlock = await rpcProvider.getBlockNumber();
@@ -31,36 +32,44 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       let toBlock = block + FILTER_BLOCK_STEP;
       if (toBlock > endBlock) toBlock = endBlock;
 
+      i++;
       const logs = await rpcProvider.getLogs({
         fromBlock: block,
         toBlock: toBlock,
         address: eventContract.address,
-        topics: [topicIds[eventType]]
+        topics: [topicIds[eventType]],
       });
 
-      results.push(...logs.filter(log => !log.removed).map(log => (
-        eventType === 'deposit' ? {
-          hash: log.transactionHash,
-          blockNumber: log.blockNumber,
-          account: convertFromHexString(log.topics[1]),
-          token: convertFromHexString(log.topics[2]),
-          amount: BigNumber.from(
-            ethers.utils.hexStripZeros(log.topics[3]),
-          ).toString()
-        } : {
-          hash: log.transactionHash,
-          blockNumber: log.blockNumber,
-          token: convertFromHexString(log.topics[1]),
-          amount: BigNumber.from(
-            ethers.utils.hexStripZeros(log.topics[2]),
-          ).toString()
-        }
-      )));
+      results.push(
+        ...logs
+          .filter((log) => !log.removed)
+          .map((log) =>
+            eventType === "deposit"
+              ? {
+                  hash: log.transactionHash,
+                  blockNumber: log.blockNumber,
+                  account: convertFromHexString(log.topics[1]),
+                  token: convertFromHexString(log.topics[2]),
+                  amount: BigNumber.from(
+                    ethers.utils.hexStripZeros(log.topics[3])
+                  ).toString(),
+                }
+              : {
+                  hash: log.transactionHash,
+                  blockNumber: log.blockNumber,
+                  token: convertFromHexString(log.topics[1]),
+                  amount: BigNumber.from(
+                    ethers.utils.hexStripZeros(log.topics[2])
+                  ).toString(),
+                }
+          )
+      );
     }
 
+    console.log("event calls", eventType, i);
     return res.status(200).json(results);
   } catch (error) {
-    console.error('Get event error', eventType, error);
+    console.error("Get event error", eventType, error);
     return res.json({});
   }
 };
