39757bb13da2e977e3d8e40ca7682b1d3aab0c3a
anthowen <anthowen@users.noreply.github.com>
2023-08-16 21:22:49 -0400

    chore(app): Use custom RPC provider

--- packages/app/src/providers/wagmi.ts
+++ packages/app/src/providers/wagmi.ts
@@ -1,26 +1,34 @@
-import { getDefaultWallets } from '@rainbow-me/rainbowkit'
-import { configureChains, createClient } from 'wagmi'
-import { polygonMumbai } from 'wagmi/chains'
-import { publicProvider } from 'wagmi/providers/public'
-import { SITE_NAME } from '../utils/config'
+import { getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { Chain, configureChains, createClient } from "wagmi";
+import { polygonMumbai } from "wagmi/chains";
+import { SITE_NAME } from "../utils/config";
+import { ethers } from "ethers";
 
 const { chains, provider, webSocketProvider } = configureChains(
   [polygonMumbai],
   [
-    publicProvider(),
-  ],
-)
+    (chain: Chain) => ({
+      chain,
+      provider: () => {
+        return new ethers.providers.StaticJsonRpcProvider(
+          process.env.NEXT_PUBLIC_RPC_URL!,
+          polygonMumbai.id
+        );
+      },
+    }),
+  ]
+);
 
 const { connectors } = getDefaultWallets({
   appName: SITE_NAME,
   chains,
-})
+});
 
 export const client = createClient({
   autoConnect: true,
   connectors,
   provider,
   webSocketProvider,
-})
+});
 
-export { chains }
+export { chains };
