dc5004c181ac60e9f91f4a67d76db6c314e5a3c1
anthowen <anthowen@users.noreply.github.com>
2023-08-16 20:31:28 -0400

    chore: Filter from the latest block

--- packages/contracts/index.ts
+++ packages/contracts/index.ts
@@ -4,15 +4,16 @@ import MockUSDTJson from './deployments/mumbai/MockUSDT.json';
 import MockUSDCJson from './deployments/mumbai/MockUSDC.json';
 
 export const ADDRESSES = {
-  CrowdfundingEvent: CrowdfundingEventJson.address,
-  MockUSDT: MockUSDTJson.address,
-  MockUSDC: MockUSDCJson.address
+	CrowdfundingEvent: CrowdfundingEventJson.address,
+	MockUSDT: MockUSDTJson.address,
+	MockUSDC: MockUSDCJson.address,
 } as const;
 
 export const ABIS = {
-  CrowdfundingEvent: CrowdfundingEventJson.abi,
-  MockUSDT: MockUSDTJson.abi,
-  MockUSDC: MockUSDCJson.abi
+	CrowdfundingEvent: CrowdfundingEventJson.abi,
+	MockUSDT: MockUSDTJson.abi,
+	MockUSDC: MockUSDCJson.abi,
 } as const;
 
-export const DEPLOY_BLOCK = CrowdfundingEventJson.receipt.blockNumber;
+// export const DEPLOY_BLOCK = CrowdfundingEventJson.receipt.blockNumber;
+export const DEPLOY_BLOCK = 39074180;
